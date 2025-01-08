2de7216380a5b08c3c2fc6ed16466f68e78befb2
louisprvst <louis.prevost1610@gmail.com>
2025-01-08 14:05:10 +0100

    Merge entre le jeu et le code modifié

--- client.c
+++ client.c
@@ -1,6 +1,6 @@
-/*
+/* 
 	##################################################################
-	### Project Socket by PREVOST Louis | SEYS Anthony | LIM Julie ###
+	### Project Socket by PREVOST Louis | SEYS Anthony | LIM Julie ### 
 	##################################################################
 */
 
@@ -9,38 +9,41 @@
 #include <unistd.h> /* pour read, write, close, sleep */
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>		/* pour memset */
+#include <string.h> /* pour memset */
 #include <netinet/in.h> /* pour struct sockaddr_in */
-#include <arpa/inet.h>	/* pour htons et inet_aton */
+#include <arpa/inet.h> /* pour htons et inet_aton */
 
 #define LG_MESSAGE 256
 
-void creation_socket_client(int argc, char *argv[], char *ip_dest, int *port_dest, int *descripteurSocket)
-{
-	if (argc > 1)
-	{ // si il y a au moins 2 arguments passés en ligne de commande, récupération ip et port
-		strncpy(ip_dest, argv[1], 16);
-		sscanf(argv[2], "%d", port_dest);
-	}
-	else
-	{
-		printf("USAGE : %s ip port\n", argv[0]);
+void affiche(char grille[LG_MESSAGE]){
+	printf("~~~~~~~~~~~~~~~~~~~~\n");
+	printf(" %c | %c | %c\n" , grille[0], grille[1], grille[2]);
+	printf("------------\n");
+	printf(" %c | %c | %c\n" , grille[3], grille[4], grille[5]);
+	printf("------------\n");
+	printf(" %c | %c | %c\n" , grille[6], grille[7], grille[8]);
+}
+
+void creation_socket_client(int argc, char *argv[], char *ip_dest, int *port_dest, int *descripteurSocket) {
+	if (argc>1) { // si il y a au moins 2 arguments passés en ligne de commande, récupération ip et port
+		strncpy(ip_dest,argv[1],16);
+		sscanf(argv[2],"%d",port_dest);
+	}else{
+		printf("USAGE : %s ip port\n",argv[0]);
 		exit(-1);
 	}
 
 	// Crée un socket de communication
 	*descripteurSocket = socket(AF_INET, SOCK_STREAM, 0);
 	// Teste la valeur renvoyée par l’appel système socket()
-	if (*descripteurSocket < 0)
-	{
+	if(*descripteurSocket < 0){
 		perror("Erreur en création de la socket..."); // Affiche le message d’erreur
-		exit(-1);									  // On sort en indiquant un code erreur
+		exit(-1); // On sort en indiquant un code erreur
 	}
 	printf("Socket créée! (%d)\n", *descripteurSocket);
 }
 
-void attente_client(char *ip_dest, int port_dest, int descripteurSocket, struct sockaddr_in *sockaddrDistant, socklen_t *longueurAdresse)
-{
+void attente_client(char *ip_dest, int port_dest, int descripteurSocket, struct sockaddr_in *sockaddrDistant, socklen_t *longueurAdresse) {
 	// Remplissage de sockaddrDistant (structure sockaddr_in identifiant la machine distante)
 	// Obtient la longueur en octets de la structure sockaddr_in
 	*longueurAdresse = sizeof(struct sockaddr_in);
@@ -56,78 +59,119 @@ void attente_client(char *ip_dest, int port_dest, int descripteurSocket, struct 
 	inet_aton(ip_dest, &sockaddrDistant->sin_addr);
 
 	// Débute la connexion vers le processus serveur distant
-	if ((connect(descripteurSocket, (struct sockaddr *)sockaddrDistant, *longueurAdresse)) == -1)
-	{
+	if((connect(descripteurSocket, (struct sockaddr *)sockaddrDistant, *longueurAdresse)) == -1){
 		perror("Erreur de connection avec le serveur distant...");
 		close(descripteurSocket);
 		exit(-2); // On sort en indiquant un code erreur
 	}
-	printf("Connexion au serveur %s:%d réussie!\n", ip_dest, port_dest);
+	printf("Connexion au serveur %s:%d réussie!\n",ip_dest,port_dest);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 
 	char buffer[LG_MESSAGE]; // buffer stockant le message
-	int nb;					 /* nb d’octets écrits et lus */
+	int nb; /* nb d’octets écrits et lus */	
 	char ip_dest[16];
-	int port_dest;
+	int  port_dest;
 	int descripteurSocket;
 	socklen_t longueurAdresse;
 	struct sockaddr_in sockaddrDistant;
+
+	int grille[9] ;
 
 	// Pour pouvoir contacter le serveur, le client doit connaître son adresse IP et le port de comunication
 	// Ces 2 informations sont passées sur la ligne de commande
 	// Si le serveur et le client tournent sur la même machine alors l'IP locale fonctionne : 127.0.0.1
 	// Le port d'écoute du serveur est 5000 dans cet exemple, donc en local utiliser la commande :
 	// ./client_base_tcp 127.0.0.1 5000
+	
 
 	// Gestion de la connexion du client > serveur
 
 	creation_socket_client(argc, argv, ip_dest, &port_dest, &descripteurSocket);
-	// longueurAdresse = sizeof(struct sockaddr_in);
+	// longueurAdresse = sizeof(struct sockaddr_in); 
 	attente_client(ip_dest, port_dest, descripteurSocket, &sockaddrDistant, &longueurAdresse);
+
 
 	// Gestion intéractions
 
-	while (1)
+	while(1)
 	{
 		printf("\nChoisissez l'une des options suivantes : \n");
 		printf("° Tapez 'quit', si vous souhaitez arrêter l'applications ! \n");
+		printf("° Tapez 'play' pour jouer ! \n");
 		printf("\n> ");
 		fgets(buffer, LG_MESSAGE, stdin);
-		buffer[strcspn(buffer, "\n")] = '\0';
+		buffer[strcspn(buffer, "\n")] = '\0'; 
 
 		// Envoi du message
-		// switch(nb = write(descripteurSocket, buffer, strlen(buffer))){
-		switch (nb = send(descripteurSocket, buffer, strlen(buffer) + 1, 0))
-		{
-		case -1: /* une erreur ! */
-			perror("Erreur en écriture...");
-			close(descripteurSocket);
-			exit(-3);
-		case 0: /* le socket est fermée */
-			fprintf(stderr, "Le socket a été fermée par le serveur !\n\n");
-			return 0;
-		default: /* envoi de n octets */
-			printf("Message %s envoyé! (%d octets)\n\n", buffer, nb);
-		}
-		if (strcmp(buffer, "shutdown") == 0)
-		{
-			// On ferme la ressource avant de quitter
-			close(descripteurSocket);
-			return 0;
-		}
-		else
-		{
-			printf("Réponse du serveur : %s\n", buffer);
+		//switch(nb = write(descripteurSocket, buffer, strlen(buffer))){
+		switch(nb = send(descripteurSocket, buffer, strlen(buffer)+1,0)){
+			case -1 : /* une erreur ! */
+					perror("Erreur en écriture...");
+					close(descripteurSocket);
+					exit(-3);
+			case 0 : /* le socket est fermée */
+				fprintf(stderr, "Le socket a été fermée par le serveur !\n\n");
+				return 0;
+			default: /* envoi de n octets */
+				printf("Message %s envoyé! (%d octets)\n\n", buffer, nb);
 		}
 		nb = recv(descripteurSocket, buffer, LG_MESSAGE, 0);
-		if (nb < 0)
-		{
+		if(nb < 0){
 			perror("Erreur de réception...");
 			close(descripteurSocket);
 			exit(-4);
 		}
+		printf("Réponse du serveur : %s\n", buffer);
+
+		if(strcmp(buffer, "Fermeture") == 0) {
+			// On ferme la ressource avant de quitter
+			close(descripteurSocket);
+			return 0;
+		} 
+
+		if(strcmp(buffer, "Start") == 0) {
+			while(1){
+				printf("A votre tour :\n");
+
+				// Pour recevoir et enregistrer le choix
+				memset(buffer, 0x00, LG_MESSAGE);
+				recv(descripteurSocket, buffer, LG_MESSAGE, 0);
+				printf("%s",buffer);
+
+				int choix;
+				scanf("%d", &choix);
+
+				// Pour transformer choix en texte
+				sprintf(buffer, "%d", choix) ;
+
+				//Envoie et check les erreurs
+				switch(nb = send(descripteurSocket, buffer, strlen(buffer) + 1, 0)){
+					case -1 :
+							perror("Erreur en écriture...");
+							close(descripteurSocket);
+							exit(-3);
+					case 0 :
+						fprintf(stderr, "Le socket a été fermée par le serveur !\n\n");
+						return 0;
+				}
+
+				// Pour recevoir et afficher
+				memset(buffer, 0x00, LG_MESSAGE);
+				recv(descripteurSocket, buffer, 9, 0);
+				affiche(buffer);
+
+				printf("L'ordinateur va jouer :\n");
+
+				// Pour recevoir et afficher
+				memset(buffer, 0x00, LG_MESSAGE);
+				recv(descripteurSocket, buffer, 9, 0);
+				affiche(buffer);
+			}
+			return 0;
+		} 
 	}
+	return 0 ;
 }
+
--- serveur.c
+++ serveur.c
@@ -17,6 +17,72 @@
 
 #define LG_MESSAGE 256
 
+void init_grille(char grille[9]){
+	for(int i = 0 ; i < 9 ; i++){
+			grille[i] = ' ' ;
+		}
+}
+
+void affiche(char grille[9]){
+	printf("~~~~~~~~~~~~~~~~~~~~\n");
+	printf(" %c | %c | %c\n" , grille[0], grille[1], grille[2]);
+	printf("------------\n");
+	printf(" %c | %c | %c\n" , grille[3], grille[4], grille[5]);
+	printf("------------\n");
+	printf(" %c | %c | %c\n" , grille[6], grille[7], grille[8]);
+}
+
+void prendre_case(char grille[9], char *messageRecu) {
+    int caseNum;
+
+    sscanf(messageRecu, "%d", &caseNum);
+
+	if(grille[caseNum - 1] == ' '){
+		switch (caseNum) {
+			case 1:  
+				grille[0] = 'X'; 
+				break;
+			case 2:  
+				grille[1] = 'X'; 
+				break;
+			case 3:  
+				grille[2] = 'X'; 
+				break;
+			case 4:  
+				grille[3] = 'X'; 
+				break ; 
+			case 5:  
+				grille[4] = 'X'; 
+				break;
+			case 6:  
+				grille[5] = 'X'; 
+				break;
+			case 7:  
+				grille[6] = 'X'; 
+				break;
+			case 8:  
+				grille[7] = 'X'; 
+				break;
+			case 9:  
+				grille[8] = 'X'; 
+				break;
+			default:
+				printf("Option invalide.\n");
+				return;
+    	}
+	}
+}
+
+int bot_player(char grille[9]) {
+	for(int i = 0 ; i < 8 ; i++){
+		if(grille[i] == ' '){
+			grille[i] = 'O';
+			return 0;
+		}
+	}
+	return 1 ;
+}
+
 void creation_socket(int *socketEcoute)
 {
 
@@ -71,8 +137,12 @@ int main(int argc, char *argv[])
 	int socketDialogue;
 	struct sockaddr_in pointDeRencontreDistant;
 	char messageRecu[LG_MESSAGE]; /* le message de la couche Application ! */
+	char messageEnvoye[LG_MESSAGE];
 	int ecrits, lus;			  /* nb d’octets ecrits et lus */
 	int retour;
+
+	char grille[9] ;
+	init_grille(grille);
 
 	// Gestion des socket
 
@@ -81,22 +151,19 @@ int main(int argc, char *argv[])
 
 	// T3N
 
-	while (1)
-	{
+	while (1) {
 		memset(messageRecu, 0, LG_MESSAGE * sizeof(char));
 		printf("Attente d’une demande de connexion (quitter avec Ctrl-C)\n\n");
 
 		socketDialogue = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, &longueurAdresse);
-		if (socketDialogue < 0)
-		{
+		if (socketDialogue < 0){
 			perror("accept");
 			close(socketDialogue);
 			close(socketEcoute);
 			exit(-4);
 		}
-		while (1)
-		{
-			lus = recv(socketDialogue, messageRecu, LG_MESSAGE * sizeof(char), 0);
+
+		lus = recv(socketDialogue, messageRecu, LG_MESSAGE * sizeof(char), 0);
 			switch (lus)
 			{
 			case -1:
@@ -114,8 +181,36 @@ int main(int argc, char *argv[])
 			// Réponse selon le message reçu
 			if (strncmp(messageRecu, "quit", 4) == 0)
 			{
-				send(socketDialogue, "shutdown", strlen("shutdown") + 1, 0);
+				send(socketDialogue, "Fermeture", strlen("Fermeture") + 1, 0);
 				break;
+			}
+			if (strncmp(messageRecu, "play", 4) == 0)
+			{
+				send(socketDialogue, "Start", strlen("Start") + 1, 0);
+				while (1){
+
+					// Pour envoyer
+					memset(messageEnvoye, 0x00, LG_MESSAGE);
+					snprintf(messageEnvoye, LG_MESSAGE, "Numéro de la case que vous souhaitez prendre : (1 à 9) :");
+					send(socketDialogue, messageEnvoye, strlen(messageEnvoye) + 1, 0);
+
+					// Pour recup
+					memset(messageRecu, 0x00, LG_MESSAGE);
+					lus = recv(socketDialogue, messageRecu, LG_MESSAGE, 0);
+
+					// On verifie , on claim la case et on envoie au joueur
+					if (lus > 0) {
+						printf("Message reçu : %s\n", messageRecu);
+						prendre_case(grille, messageRecu);
+						affiche(grille);
+						send(socketDialogue, grille, sizeof(grille), 0);
+					}
+
+					// au tour du serveur de jouer
+					bot_player(grille);
+					affiche(grille);
+					send(socketDialogue, grille, sizeof(grille), 0);
+				}
 			}
 			else
 			{
@@ -123,7 +218,7 @@ int main(int argc, char *argv[])
 				send(socketDialogue, erreur, strlen(erreur) + 1, 0);
 				printf("Commande inconnue envoyée au client.\n");
 			}
-		}
+			
 		close(socketDialogue);
 	}
 
@@ -131,3 +226,4 @@ int main(int argc, char *argv[])
 	close(socketEcoute);
 	return 0;
 }
+
